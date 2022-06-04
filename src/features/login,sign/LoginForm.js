b332c0d6e1a1613b85c7e78f49e7684c5834d1ff
taehyeong park <b_tae96@naver.com>
2022-06-04 19:56:38 +0900

    update. 입력 값 없을 때 버튼 막기 작성

--- src/features/login,sign/LoginForm.js
+++ src/features/login,sign/LoginForm.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "../../elem/Button";
 import Input from "../../elem/Input";
 import Text from "../../elem/Text";
 
 export default function LoginForm() {
+  const [check, setCheck] = useState(true);
+  const [dataList, setDataList] = useState({
+    email: "",
+    password: "",
+  });
   const login = (e) => {
     e.preventDefault();
-    alert("bb");
   };
+  const inputChange = (e) => {
+    const { value, id } = e.target;
+    setDataList({ ...dataList, [id]: value });
+  };
+
+  React.useEffect(() => {
+    if (dataList.email !== "" && dataList.password !== "") {
+      setCheck(false);
+    } else {
+      setCheck(true);
+    }
+  }, [dataList]);
   return (
     <>
       <WrapForm onSubmit={login}>
-        <Label htmlFor="id">아이디</Label>
-        <Input type="email" id="id" required />
+        <Label htmlFor="email">아이디</Label>
+        <Input type="email" id="email" required onChange={inputChange} />
         <Label htmlFor="password">비밀번호</Label>
-        <Input type="password" id="password" required />
-        <Button size="size1" color="white" bgcolor="blue">
+        <Input type="password" id="password" required onChange={inputChange} />
+        <Button
+          size="size1"
+          color="white"
+          bgcolor={check ? "grey" : "blue"}
+          disabled={check}
+        >
           <Text font="head02">로그인하기</Text>
         </Button>
       </WrapForm>
--- src/features/login,sign/SignForm.js
+++ src/features/login,sign/SignForm.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "../../elem/Button";
 import Input from "../../elem/Input";
 import Text from "../../elem/Text";
 
 export default function SignForm() {
+  const [check, setCheck] = useState(true);
+  const [dataList, setDataList] = useState({
+    email: "",
+    nick: "",
+    password: "",
+    passwordCheck: "",
+  });
   const sign = (e) => {
     e.preventDefault();
-    alert("bb");
   };
+
+  const inputChange = (e) => {
+    const { value, id } = e.target;
+    setDataList({ ...dataList, [id]: value });
+  };
+
+  React.useEffect(() => {
+    if (
+      dataList.email !== "" &&
+      dataList.password !== "" &&
+      dataList.passwordCheck !== "" &&
+      dataList.nick !== ""
+    ) {
+      setCheck(false);
+    } else {
+      setCheck(true);
+    }
+  }, [dataList]);
   return (
     <>
       <WrapForm onSubmit={sign}>
-        <Label htmlFor="id">아이디</Label>
-        <Input type="email" id="id" required />
+        <Label htmlFor="email">아이디</Label>
+        <Input type="email" id="email" required onChange={inputChange} />
         <Label htmlFor="nick">닉네임</Label>
-        <Input type="text" id="nick" required />
+        <Input type="text" id="nick" required onChange={inputChange} />
         <Label htmlFor="password">비밀번호</Label>
-        <Input type="password" id="password" required />
+        <Input type="password" id="password" required onChange={inputChange} />
         <Label htmlFor="passwordCheck">비밀번호 확인</Label>
-        <Input type="password" id="passwordCheck" required />
-        <Button size="size1" color="white" bgcolor="blue">
+        <Input
+          type="password"
+          id="passwordCheck"
+          required
+          onChange={inputChange}
+        />
+        <Button
+          size="size1"
+          color="white"
+          bgcolor={check ? "grey" : "blue"}
+          disabled={check}
+        >
           <Text font="head02">회원가입하기</Text>
         </Button>
       </WrapForm>
